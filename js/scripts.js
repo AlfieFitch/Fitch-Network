2f0da3acaaf31942547b5e34482715ce8bb5fb06
Alfie <92184@farnborough.ac.uk>
2021-02-20 00:16:23 +0000

    V1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 //Array of images which you want to show: Use path you want.
-var images=new Array('./imgs/background/avengers.jpg','./imgs/background/coco.jpg','./imgs/background/mando.jpg','./imgs/background/moana.jpg','./imgs/background/tenet.jpg','./imgs/background/ww_bg.jpg');
-var nextimage=0;
-doSlideshow();
+var images=new Array('./css/imgs/background/avengers.jpg','./css/imgs/background/coco.jpg','./css/imgs/background/mando.jpg','./css/imgs/background/moana.jpg','./css/imgs/background/tenet.jpg','./css/imgs/background/ww_bg.jpg');
+let background = 0;
 
-function doSlideshow(){
-    if(nextimage>=images.length){nextimage=0;}
-    $('.images-slide')
-    .css('background-image:','url("'+images[nextimage++]+'")')
-    .fadeIn(500,function(){
-        setTimeout(doSlideshow,1000);
-    });
+
+window.onload = function() {
+        background = Math.floor(Math.random() * 6);
+        document.getElementById('images-slide').style.backgroundImage="linear-gradient(to bottom, rgba(11, 23, 61, 0.685), rgb(11,23,61,1)), url("+ images[background] + ")";
+        console.log(background);
 }
